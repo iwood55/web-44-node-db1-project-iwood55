31d7ac972bfdd0b42cd3362e91bbf558b44e93de
Isaac Ellingwood <81526595+iwood55@users.noreply.github.com>
2021-08-09 18:53:41 -0400

    mvp

--- api/server.js
+++ api/server.js
@@ -2,6 +2,24 @@ const express = require("express");
 
 const server = express();
 
+const accountsRouter = require('./accounts/accounts-router');
+
+const errorHandling = (err, req, res, next) => {
+    const status = err.status || 500
+    res.status(status).json({
+        message: err.message,
+})
+}
+
+const notFound = (req, res, next) => {
+    res.status(404).json({
+        message: 'Not found'
+})
+}
+
 server.use(express.json());
+server.use('/api/accounts', accountsRouter)
+server.use(errorHandling)
+server.use('*', notFound)
 
 module.exports = server;
